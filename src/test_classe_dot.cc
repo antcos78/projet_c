6b553310a645a7f711881519d5351cf66ef315bf
leo michel <leomichel.om@gmail.om>
2020-12-10 11:10:54 +0100

    test dot item

--- src/test_classe_dot.cc
+++ src/test_classe_dot.cc
@@ -7,6 +7,7 @@
 #include "../inc/Porte_2.h"
 #include "../inc/And2.h"
 #include "../inc/Or2.h"
+#include "../inc/Dot.h"
 
 using namespace std;
 
@@ -16,31 +17,14 @@ int main ()
 {
   cout << "salut" << endl;
 
-  And2 salut("porte_AND2",2,26);
-  Not notnot("not2",1,1);
-  And2 tigre("porte",2,2);
+  Dot parsingDot;
 
-  salut.setEtat_1(0);
-  salut.setEtat_2(0);
-  cout << "Etat avec etat_1 " << salut.getEtat_1() << " et etat2 " << salut.getEtat_2() <<": " << salut.calculEtat() << endl;
-  salut.setEtat_1(0);
-  salut.setEtat_2(1);
-  cout << "Etat avec etat_1 0 et etat2 1: " << salut.calculEtat() << endl;
-  salut.setEtat_1(1);
-  salut.setEtat_2(0);
-  cout << "Etat avec etat_1 1 et etat2 0: " << salut.calculEtat() << endl;
-  salut.setEtat_1(1);
-  salut.setEtat_2(1);
-  cout << "Etat avec etat_1 1 et etat2 1: " << salut.calculEtat() << endl;
+  Not salut("porte",1,1);
 
+  parsingDot.addItems("porte", &salut);
+  cout << parsingDot.trouverItemsParNom("porte")->getNom() << endl;
+  cout << parsingDot.trouverItemsParNumero(0)->getNom() << endl;
 
-  salut.ajoutOutput(notnot.getNom());
-  salut.ajoutOutput(tigre.getNom());
-  cout << "en sortie : " << salut.getOutput(0) << endl;
-  cout << "en sortie : " << salut.getOutput(1) << endl;
-  cout << "en sortie : " << salut.getNbOutput() << endl;
-
-  cout << "en sortie : " << salut.getOutput(2) << endl;
 
 
   return 0;
